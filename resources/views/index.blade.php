327a9a8a739caac06e4a497b10bb100800e623d3
brix <brix@nice.com>
2022-01-19 19:27:17 -0600

    view principal finalizada

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,22 +11,21 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Google News</title>
-
+   <style>
+       .div_centrado{
+           margin: 0 auto;
+       }
+   </style>
 </head>
 
 <body>
-    <nav class="navbar sticky-top navbar-light bg-light fixed-top">
-        <div class="container-fluid">
-            <a class="navbar-toggler btn btn-light" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+    <nav class="navbar sticky-top navbar-light bg-light border-bottom">
+        <div class="d-flex justify-content-start align-items-center">
+            <a class="btn btn-light" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
                 <span><i class="fas fa-bars fa-lg"></i></span>
             </a>
-            <a class="navbar-brand" href="#">Google News</a>
-            <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            </form>
-
-            <a class="btn btn-light" href="{{route('agregar')}}"><i class="fas fa-plus-circle fa-lg"></i></a>
-
+            <a class="navbar-brand" href="{{ route('noticias') }}"><img src="https://upload.wikimedia.org/wikipedia/commons/3/33/Google_News.png" height="40" width="110"></a>
+            
             <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                 <div class="offcanvas-body">
                     <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
@@ -55,7 +54,21 @@
                     </ul>
                 </div>
             </div>
+        </div>   
+        <div class="d-flex  align-items-center">
+            <div class="container">
+                <div class="row" >
+                    <div class="col-md-23">
+                        <input class="form-control " type="text" placeholder='Buscar' aria-label="Search" >
+                    </div>
+                </div>
+            </div>
         </div>
+        <div class="d-flex justify-content-end align-items-center">
+            <a href="#" class="btn btn-light"><i class="fas fa-th"></i></a>
+        <a class="btn btn-light" href="{{route('agregar')}}"><i class="fas fa-plus-circle fa-lg"></i></a>
+        </div>
+        
     </nav>
 
     @yield('content')
